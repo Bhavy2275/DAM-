07e6360ac8b3bfc0ccaca063a8431a6e563de792
bhavy <sonibhavya2004@gmail.com>
2026-03-29 21:47:40 +0530

    SECURITY: Remove leaked test-db.js containing hardcoded credentials

--- server/test-db.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: 'postgresql://postgres.mopehzmgrcrmevbafzsq:a12g6UMCq9lEPQuf@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres?pgbouncer=true'
-    }
-  }
-});
-prisma.$queryRawUnsafe('SELECT 1').then(() => console.log('CONNECTION OK')).catch(e => console.error('CONNECTION ERROR:', e.message)).finally(() => prisma.$disconnect());
